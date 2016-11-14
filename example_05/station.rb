10fa09b2dda661d4863066424fa95f372458dcad
R.G <liman.vita@gmail.com>
2016-11-15 00:28:00 +0300

    Class Station

---
+++ example_05/station.rb
@@ -0,0 +1,28 @@
+class Station
+  def initialize(name)
+    @name = name
+    @train_list = []
+  end
+
+  def add_train(train)
+    @train_list.push(train)
+  end
+
+  def list_train
+    @train_list
+  end
+
+  def del_train
+    @train_list.delete_at(-1)
+  end
+
+  def sorting_for_type
+    pass = @train_list.select { |train| train.type == :p }
+    puts "Кол-во пассажирских поездов #{pass.count}"
+    pass.each { |train| puts train.number }
+
+    cargo = @train_list.select { |train| train.type == :c }
+    puts "Кол-во грузовых поездов #{cargo.count}"
+    cargo.each { |train| puts train.number }
+  end
+end
