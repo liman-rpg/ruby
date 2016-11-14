2dad615f39ecb78901db8165ed096e23a7908951
R.G <liman.vita@gmail.com>
2016-11-14 13:38:14 +0300

    Ver. 3

--- example_04/each.rb
+++ example_04/each.rb
@@ -1,13 +1,16 @@
 puts "Введите число"
 number = gets.chomp.to_i
 
+puts "Введите второе число"
+number2 = gets.chomp.to_i
+
 arr = Array.new(number) { |i| i+1 }
 puts "#{arr}"
 
 sum = 0
 
 (1..number).each do |i|
-  sum += i if i%2 == 0
+  sum += i if i%number2 == 0
   puts "#{sum}"
 end
-puts "Сумма только четных от 1 до введенного числа = #{sum}"
+puts "Сумма от 1 до введенного числа = #{sum}"
