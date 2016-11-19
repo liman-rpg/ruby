da976c8c73f1813fecf44b9312481ba4a2555f91
R.G <liman.vita@gmail.com>
2016-11-20 00:24:23 +0300

    Init

---
+++ example_06/route.rb
@@ -0,0 +1,22 @@
+class Route
+  attr_reader :route_list, :last_station
+
+  def initialize(first_station, last_station)
+    @first_station = first_station
+    @last_station = last_station
+    @intermediate_station = []
+    @route_list = [@first_station, @intermediate_station, @last_station]
+  end
+
+  def add_station(station)
+    @intermediate_station.push(station)
+  end
+
+  def del_station(station)
+    @intermediate_station.delete(station)
+  end
+
+  def list_station
+    @route_list
+  end
+end
