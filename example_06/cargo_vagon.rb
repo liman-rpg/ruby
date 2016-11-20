5db0e0f9ee31a6b1ecb3d4ddec5c312124a24254
R.G <liman.vita@gmail.com>
2016-11-20 14:27:02 +0300

    Add validation for CargoVagon + refactoring

--- example_06/cargo_vagon.rb
+++ example_06/cargo_vagon.rb
@@ -6,14 +6,21 @@ class CargoVagon < Vagon
   def initialize(size)
     @max_loading = size
     @free_place = size
+    valid?
   end
 
   def loading_cargo(load)
-    @free_place -= load if load <= @free_place
+    raise ArgumentError, "Input 'load' must be greater than 0" if load < 0
+    raise ArgumentError, "Input is large" unless load <= @free_place
+
+    @free_place -= load
   end
 
   def free_place(load)
-    @free_place += load if load <= @max_loading - @free_place
+    raise ArgumentError, "Input 'load' must be greater than 0" if load < 0
+    raise ArgumentError, "Input is large" unless load <= @max_loading - @free_place
+
+    @free_place += load
   end
 
   def size_loading_cargo
@@ -22,5 +29,14 @@ class CargoVagon < Vagon
 
   def size_free_cargo
     @free_place
+  end
+
+  private
+
+  def valid?
+    raise ArgumentError, "Size can't be blank" if @max_loading.nil? || @free_place.nil?
+    raise ArgumentError, "Input Size error. The Size is not Integer" unless @max_loading.is_a?(Integer) || @free_place.is_a?(Integer)
+    raise ArgumentError, "Input Size error. The Size must be greater than 0" if @max_loading <= 0 || @free_place <= 0
+    true
   end
 end
