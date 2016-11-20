eb0260d7ade09fe7058354726c94c3855448884b
R.G <liman.vita@gmail.com>
2016-11-20 14:35:51 +0300

    Add validation for PassengerVagon

--- example_06/passenger_vagon.rb
+++ example_06/passenger_vagon.rb
@@ -6,14 +6,19 @@ class PassengerVagon < Vagon
   def initialize(size)
     @max_places = size
     @free_places = size
+    valid?
   end
 
   def take_place
-    @free_places -= 1 if @free_places > 0
+    raise ArgumentError, "Do not have Free plases" if @free_places <= 0
+
+    @free_places -= 1
   end
 
   def free_place
-    @free_places += 1 if @free_places < @max_places
+    raise ArgumentError, "Max count Free plases" unless @free_places < @max_places
+
+    @free_places += 1
   end
 
   def size_take_place
@@ -22,5 +27,14 @@ class PassengerVagon < Vagon
 
   def size_free_place
     @free_places
+  end
+
+  private
+
+  def valid?
+    raise ArgumentError, "Size can't be blank" if @max_places.nil? || @free_places.nil?
+    raise ArgumentError, "Input Size error. The Size is not Integer" unless @max_places.is_a?(Integer) || @free_places.is_a?(Integer)
+    raise ArgumentError, "Input Size error. The Size must be greater than 0" if @max_places <= 0 || @free_places <= 0
+    true
   end
 end
