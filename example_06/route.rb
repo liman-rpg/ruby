23f18214249a1a98b6f9741aa5132fd9c5cad53b
R.G <liman.vita@gmail.com>
2016-11-23 09:34:19 +0300

    Add exceptions for add/del methods

--- example_06/route.rb
+++ example_06/route.rb
@@ -1,3 +1,5 @@
+require_relative "station"
+
 class Route
   attr_reader :route_list, :last_station
 
@@ -10,11 +12,13 @@ class Route
   end
 
   def add_station(station)
+    raise ArgumentError, "Input Station error. The Station is not Station::Class " unless station.is_a?(Station)
     raise ArgumentError, "Stations alrady exists" if @route_list.index(station) || @intermediate_station.index(station)
     @intermediate_station.push(station)
   end
 
   def del_station(station)
+    raise ArgumentError, "Input Station error. The Station is not Station::Class " unless station.is_a?(Station)
     @intermediate_station.delete(station)
   end
 
@@ -26,7 +30,8 @@ class Route
 
   def valid?
     raise ArgumentError, "Stations can't be blank" if @first_station.nil? || @last_station.nil?
-    raise ArgumentError, "Input Station error. The Station is not Object" unless @first_station.is_a?(Object) || @last_station.is_a?(Object)
+    raise ArgumentError, "Input First Station error. The Station is not Station::Class " unless @first_station.is_a?(Station)
+    raise ArgumentError, "Input Last Station error. The Station is not Station::Class " unless @last_station.is_a?(Station)
     raise ArgumentError, "First station equal Last station" if @first_station == @last_station
     true
   end
