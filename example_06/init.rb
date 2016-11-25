f69aa707f708f751bf8da0a7fa8992e398994a4c
R.G <liman.vita@gmail.com>
2016-11-25 11:22:00 +0300

    Refactoring

--- example_06/init.rb
+++ example_06/init.rb
@@ -1,5 +1,9 @@
-load './train.rb'
+# load './train.rb'
 load './station.rb'
 load './route.rb'
 load './cargo_vagon.rb'
 load './passenger_vagon.rb'
+load './cargo_train.rb'
+load './passenger_train.rb'
+
+eval("st1 = Station.new('msk')")
